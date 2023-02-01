6cb487f677822333bef9247f308f3774d561e881
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-01 21:30:07 +0530

    UpdateSalaryusingStoredProcedure

--- ADOEmployeePayrollTesting/UnitTest1.cs
+++ ADOEmployeePayrollTesting/UnitTest1.cs
@@ -1,1 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ADO_Employee_Payroll;
 
+
+
+namespace ADOEmployeePayrollTesting
+{
+    [TestClass]
+    public class PayrollServiceTesting
+    {
+        EmployeeRepository employeeRepository;
+        [TestInitialize]
+        public void SetUp()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+        //Usecase 3: Update basic pay in Sql Server
+        [TestMethod]
+        [TestCategory("Using Sql Query")]
+        public void GivenUpdateQuery_ReturnOne()
+        {
+            int expected = 1;
+            int actual = employeeRepository.UpdateSalaryQuery();
+            Assert.AreEqual(actual, expected);
+        }
+        //Usecase 4: Update basic pay in Sql Server using Stored Procedure
+        [TestMethod]
+        [TestCategory("Using Sql Query")]
+        public void GivenUpdateQuery_UsingStoredProcedure_ReturnOne()
+        {
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+            int expected = 1;
+            employeeDataManager.EmployeeID = 3;
+            employeeDataManager.EmployeeName = "Rujula";
+            employeeDataManager.BasicPay = 30000000;
+            int actual = employeeRepository.UpdateSalary(employeeDataManager);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
